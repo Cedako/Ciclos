faefc50270fb6db0619d35a8ae291b35ce17ff95
Joshua <mramos29@ucol.mx>
2021-08-30 19:19:39 -0500

    Hecha la primera serie, sigue E

--- ciclos.js
+++ ciclos.js
@@ -1,22 +1,34 @@
 class Ciclos{
     constructor(){
-        this.limite = 100;
-        console.log(this.cicloS(2))
+        //El valor de la variable dicta el número de veces que se realizará el ciclo
+        console.log(this.cicloS(1))
     }
 
     cicloS(limite){
         let n = 1
         let serieS = (4/n)
-        if(limite > n){
-            for (let i = 3; i <= limite; i+=2){
-                serieS-=(4/i)
-                i+=2
+        //Si el valor es 0, no hay necesidad de un ciclo, pues el valor inicial es 4/1.
+        if(limite > 0){
+            for (let i = 0; i <= limite; i++){
                 if(i < limite){
-                    serieS+=(4/i)
+                    n+=2
+                serieS-=(4/n)
+                //console.log(n)
+                //console.log(serieS)
+                }
+                i++
+                if(i < limite){
+                    n+=2
+                    serieS+=(4/n)
+                    //console.log(n)
+                    //console.log(serieS)
                 }
             }
         }
         return serieS
+    }
+
+    cicloE(limite){
         
     }
 }
